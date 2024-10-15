587b1e3041ee429b3512b830679c1ebc22231a58
Steven Oxley <steven.aj.oxley@gmail.com>
2024-10-15 12:26:42 -0700

    Add 404 page.

--- hugo/deploy.sh
+++ hugo/deploy.sh
@@ -2,7 +2,9 @@
 
 set -Eeuo pipefail
 
-[[ -d public ]] && rm -r public
+if [[ "$#" -gt 0 && "$1" == "clean" ]]; then
+    [[ -d public ]] && rm -r public
+fi
 
 hugo
 aws s3 sync --profile soxley --delete public s3://stevenoxley-com-site
